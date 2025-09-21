e257eb3b15483344fb22d176f7348c637fd0a6b4
Quao Jonathan <165655601+joe-qodes@users.noreply.github.com>
2025-09-21 21:41:13 +0000

    Updated test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,8 +3,9 @@
 Unit tests for utils.access_nested_map.
 """
 import unittest
+from unittest import mock
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -29,6 +30,34 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
         # KeyError message should be the missing key
         self.assertEqual(str(context.exception), repr(path[-1]))
+
+
+class TestMemoize(unittest.TestCase):
+    """Unit tests for the memoize decorator."""
+
+    def test_memoize(self):
+        """Test that memoize decorator caches function results."""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with unittest.mock.patch.object(
+            TestClass, 'a_method', return_value=42
+        ) as mock_method:
+            test_obj = TestClass()
+            # First call should call the method
+            result1 = test_obj.a_property
+            # Second call should use cached result
+            result2 = test_obj.a_property
+            
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+            # Method should only be called once due to memoization
+            mock_method.assert_called_once()
 
 
 if __name__ == "__main__":
