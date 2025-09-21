29e3be4b5e08284dd3af9ab0c4ec948c38231532
Quao Jonathan <165655601+joe-qodes@users.noreply.github.com>
2025-09-21 21:49:13 +0000

    Updated test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -15,18 +15,15 @@ class TestGithubOrgClient(unittest.TestCase):
         ("google",),
         ("abc",),
     ])
-    @patch("client.get_json")
+    @patch("client.utils.get_json")
     def test_org(self, org_name, mock_get_json):
         """Test that GithubOrgClient.org returns the correct value."""
-        # Arrange
         expected = {"login": org_name}
         mock_get_json.return_value = expected
 
-        # Act
         client = GithubOrgClient(org_name)
         result = client.org
 
-        # Assert
         mock_get_json.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}"
         )
